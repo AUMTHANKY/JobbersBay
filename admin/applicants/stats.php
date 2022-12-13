c9959d2f57effc7e819e2663afbd94675ed25e3d
AUMTHANKY <64692191+AUMTHANKY@users.noreply.github.com>
2022-12-13 13:15:25 +0530

    Add files via upload

---
+++ admin/applicants/stats.php
@@ -0,0 +1,11 @@
+<html>
+    <body>
+    <thead>
+							  	<tr>
+									<th>Position</th>
+									<th>No. of applications</th>
+									<th>Report</th>
+							  	</tr>	
+							  </thead> 
+</body>
+</html>
